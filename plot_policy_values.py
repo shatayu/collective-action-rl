7bb7b34dbfeeaf91a0dc6fb77bcf3801692653e0
shatayu <shatayu@outlook.com>
2021-04-28 02:12:41 -0400

    generated charts

--- plot_policy_values.py
+++ plot_policy_values.py
@@ -5,6 +5,8 @@ import pandas as pd
 import json
 
 NUM_AGENTS = 17
+NAME = 'MADDPG Cooperative Sum State'
+FILENAME = NAME.replace(' ', '').lower()
 
 def get_policy_means(filename):
     f = open(filename, "r")
@@ -23,19 +25,16 @@ def split_obj_into_array(objects):
     
     return result
 
-objects = get_policy_means("maddpgcompetitivegamestate_policymeans.txt")
+objects = get_policy_means(FILENAME + "_policymeans.txt")
 arr = split_obj_into_array(objects)
 
 for i in range(len(arr)):
     plt.plot(arr[i])
 
-plt.savefig('maddpgcompetitivegamestate_policymeanschart.png')
-
-"""
-- add title
-- add axis labels
-- make this work for all 4
-"""
+plt.xlabel('Iteration')
+plt.ylabel('Reward')
+plt.title(NAME + ' Rewards')
+plt.savefig('charts/' + FILENAME + '_policymeanschart.png')
 
 # Data
 # df=pd.DataFrame({'x_values': range(1, 50001), 'y1_values': np.random.randn(10), 'y2_values': np.random.randn(10)+range(1,11), 'y3_values': np.random.randn(10)+range(11,21) })
