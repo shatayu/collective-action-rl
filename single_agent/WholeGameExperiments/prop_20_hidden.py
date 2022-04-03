a20aa507ffa72204f631d39df49825688bf75f9d
shatayu <shatayukulkarni@gmail.com>
2022-04-03 12:46:09 -0700

    added whole game experiments

---
+++ single_agent/WholeGameExperiments/prop_20_hidden.py
@@ -0,0 +1,17 @@
+from RLWithBushMostellerWholeGameEnv import RLWithBushMostellerWholeGameEnv
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from training_script import train_agent_for_environment
+
+REWARD_FUNCTION = 'proportion'
+NUM_ROUNDS_HIDDEN = 20
+
+env_config = {
+    'reward_function': REWARD_FUNCTION,
+    'num_rounds_hidden': NUM_ROUNDS_HIDDEN
+}
+
+experiment_name = f'{REWARD_FUNCTION}_{NUM_ROUNDS_HIDDEN}_hidden'
+
+train_agent_for_environment(RLWithBushMostellerWholeGameEnv, env_config, experiment_name)
